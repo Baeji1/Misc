c82f55a016626c539560eeb5ace149b7dbcbf8a8
Rajat Shrivastava <rajatshr@cisco.com>
2021-03-09 03:19:47 +0530

    basic sudoku class and structure

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,1 +1,24 @@
 import numpy as np
+
+
+class Sudoku:
+    def __init__(self):
+        self.rank = 3
+        self.n = self.rank ** 2
+        # self.grid = np.zeros((n,n), dtype=np.uint8) #real
+        self.grid = np.uint8(np.random.rand(self.n, self.n) * 10)
+        self.rows = np.array([x for x in self.grid])
+        self.columns = self.grid.T
+
+    def display(self):
+        print("Grid:")
+        print(self.grid)
+        print("Rows:")
+        print(self.rows)
+        print("Columns:")
+        print(self.columns)
+
+
+a = Sudoku()
+a.display()
+
